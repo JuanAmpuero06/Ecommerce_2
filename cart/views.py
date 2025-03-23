e98c0c293a7d127780503c529cdbe7f9208269b8
JuanAmpuero06 <ju.ampuero@duocuc.cl>
2025-03-23 19:31:15 -0300

    validar el stock del carrito

--- cart/views.py
+++ cart/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 from shop.models import Product
 from .models import Cart
 
@@ -12,15 +13,25 @@ def cart_detail(request):
 
 @login_required
 def add_to_cart(request, product_id):
-    """Agrega un producto al carrito"""
+    """Agrega un producto al carrito sin exceder el stock"""
     product = get_object_or_404(Product, id=product_id)
 
     # Busca si el producto ya está en el carrito del usuario
     cart_item, created = Cart.objects.get_or_create(user=request.user, product=product)
 
+    # Verificar si la cantidad en el carrito ya alcanzó el stock disponible
     if not created:
-        cart_item.quantity += 1
-        cart_item.save()
+        if cart_item.quantity < product.stock:
+            cart_item.quantity += 1
+            cart_item.save()
+        else:
+            messages.warning(request, "No puedes agregar más de la cantidad disponible en stock.")
+    else:
+        if product.stock > 0:
+            cart_item.quantity = 1  # Se inicia con 1 unidad en el carrito
+            cart_item.save()
+        else:
+            messages.warning(request, "Producto agotado. No se puede agregar al carrito.")
 
     return redirect('cart:cart_detail')
 
